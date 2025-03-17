e2241166cd1bd748d22563f1eedbf77eee5b39ba
Gomersendo Godmalin <godmalin.gomersendojr03@gmail.com>
2025-03-18 07:58:23 +0800

    models.py small adjustment

--- users/models.py
+++ users/models.py
@@ -1,44 +1,18 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from .managers import UserAccountManager
 from django.contrib.auth.models import (
-    BaseUserManager,
     AbstractBaseUser,
     PermissionsMixin
 )
-
-# Custom User Manager
-class UserAccountManager(BaseUserManager):
-    def create_user(self, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError('Users must have an email address')
-
-        email = self.normalize_email(email).lower()
-        extra_fields.setdefault('is_active', True)
-
-        user = self.model(email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, email, password=None, **extra_fields):
-        """Creates and returns a superuser with all privileges."""
-        extra_fields.setdefault("is_active", True)
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-
-        extra_fields.setdefault("is_dean", True)
-        extra_fields.setdefault("is_headdept", False)
-        extra_fields.setdefault("is_faculty", False)
-        extra_fields.setdefault("is_student", False)
-
-        return self.create_user(email, password, **extra_fields)
 
 # Custom User Model
 class UserAccount(AbstractBaseUser, PermissionsMixin):
     first_name = models.CharField(max_length=255)
     last_name = models.CharField(max_length=255)
     email = models.EmailField(unique=True, max_length=255)
+    password = models.CharField(max_length=100)
 
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
@@ -47,18 +21,18 @@ class UserAccount(AbstractBaseUser, PermissionsMixin):
     is_headdept = models.BooleanField(default=False)
     is_faculty = models.BooleanField(default=False)
     is_student = models.BooleanField(default=True)
-
+    
     objects = UserAccountManager()
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
+    REQUIRED_FIELDS = []
 
     class Meta:
         verbose_name = "User Accounts"
         verbose_name_plural = "Accounts"
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} ({self.email})"
+        return self.email
 
 #Submission and Viewing of Manuscript
 class Manuscript(models.Model):
@@ -75,7 +49,7 @@ class Manuscript(models.Model):
         ordering = ['-created_at']
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} - {self.title}"
+        return self.title
 
     @property
     def filename(self):
@@ -88,7 +62,7 @@ class Faculty(models.Model):
     department = models.CharField(max_length=255, blank=True, null=True)  # Added department field
 
     def __str__(self):
-        return f"{self.name} ({self.department})"
+        return self.name
 
 class ApplicationDefense(models.Model):
     first_name = models.CharField(max_length=255, null=True, blank=True)
@@ -120,7 +94,7 @@ class ApplicationDefense(models.Model):
         verbose_name_plural = "Student Application Defenses"
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} - {self.research_title}"
+        return self.research_title
 
 class PanelDefense(models.Model):
     first_name = models.CharField(max_length=255, blank=True, null=True)
@@ -148,4 +122,4 @@ class PanelDefense(models.Model):
         verbose_name_plural = "Student Panel Defenses"
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name} - {self.research_title}"
+        return self.research_title
